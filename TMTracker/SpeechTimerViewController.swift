e5fc1cd23a078874a43bd222ce07862a6da96476
emily404 <emily-404@hotmail.com>
2016-05-28 14:27:43 +0200

    word of day page view controller

--- TMTracker/SpeechTimerViewController.swift
+++ TMTracker/SpeechTimerViewController.swift
@@ -14,25 +14,25 @@ class SpeechTimerViewController: UIViewController {
     var counter = 0
     var timer = NSTimer()
     var secondsLeft = 450 // 07:30
-    let lightChangeInterval = 60 // in seconds
-    var speechTimeLeft = 420 // 7:00
+    let lightChangeInterval = 2 // in seconds
+    var speechTimeLeft = 10 // 7:00
     var minutesLabel = ""
     var secondsLabel = ""
     let screenSize: CGRect = UIScreen.mainScreen().bounds
     
-    @IBOutlet weak var trafficLight: UIImageView!
     @IBOutlet weak var timerLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        updateTimerLabel()
+        
         timer.invalidate()
         timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
         
-        trafficLight.backgroundColor = UIColor.greenColor()
         let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped))
-        trafficLight.userInteractionEnabled = true
-        trafficLight.addGestureRecognizer(tapGestureRecognizer)
+        self.view.userInteractionEnabled = true
+        self.view.addGestureRecognizer(tapGestureRecognizer)
 
 
     }
@@ -51,6 +51,12 @@ class SpeechTimerViewController: UIViewController {
         } else {
             timer.invalidate()
         }
+        
+        updateTimerLabel()
+        
+    }
+    
+    func updateTimerLabel() {
         minutesLabel = String(format: "%02d",(secondsLeft / 60) % 60)
         secondsLabel = String(format: "%02d", secondsLeft % 60)
         timerLabel.text = "\(minutesLabel)" + ":" + "\(secondsLabel)"
@@ -59,19 +65,21 @@ class SpeechTimerViewController: UIViewController {
     func displayLight() {
         if(speechTimeLeft > lightChangeInterval * 2) {
             // display green light
-            trafficLight.backgroundColor = UIColor.greenColor()
+            self.view.backgroundColor = UIColor.greenColor()
         } else if(speechTimeLeft > lightChangeInterval && speechTimeLeft <= lightChangeInterval * 2) {
             // display yellow light
-            trafficLight.backgroundColor = UIColor.yellowColor()
+            self.view.backgroundColor = UIColor.yellowColor()
         } else if(speechTimeLeft == 0) {
             //display red light
-            trafficLight.backgroundColor = UIColor.redColor()
+            self.view.backgroundColor = UIColor.redColor()
         }
     }
     
     func imageTapped(sender: UITapGestureRecognizer) {
         timer.invalidate()
+        
         self.navigationController?.popViewControllerAnimated(true)
+        self.navigationController?.navigationBarHidden = false
     }
     
     override func viewWillAppear(animated: Bool)
---
+++ TMTracker/WordsViewController.swift
@@ -0,0 +1,128 @@
+//
+//  WordsViewController.swift
+//  TMTracker
+//
+//  Created by Emily Chen on 2016-05-27.
+//  Copyright © 2016 Emily Chen. All rights reserved.
+//
+
+
+import UIKit
+import Alamofire
+
+class WordsViewController: UIPageViewController {
+    
+    var currentPageIndex = 0
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newColoredViewController("firstWord"),
+                self.newColoredViewController("secondWord")]
+    }()
+    
+    private func newColoredViewController(color: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewControllerWithIdentifier("\(color)ViewController")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .Forward,
+                               animated: true,
+                               completion: nil)
+        }
+        
+//        Alamofire.request(.GET, "https://httpbin.org/get", parameters: ["foo": "bar"])
+//            .responseJSON { response in
+//                print(response.request)  // original URL request
+//                print(response.response) // URL response
+//                print(response.data)     // server data
+//                print(response.result)   // result of response serialization
+//                
+//                if let JSON = response.result.value {
+//                    print("JSON: \(JSON)")
+//                }
+//        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+// MARK: UIPageViewControllerDataSource
+
+extension WordsViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        currentPageIndex = previousIndex
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        currentPageIndex = nextIndex
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    // The number of items reflected in the page indicator.
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return orderedViewControllers.count
+    }
+    
+    // The selected item reflected in the page indicator.
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return currentPageIndex
+    }
+    
+}
